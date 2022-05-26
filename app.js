57524df6dce680272a7829ad7baeaddc5dd33311
Arsen <barmany35@gmail.com>
2022-05-26 11:52:39 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const links = document.querySelectorAll(".link");
+const sections = document.querySelectorAll("section");
+const form = document.querySelector(".contact-form");
+
+let activeLink = 0;
+
+links.forEach((link, i) => {
+  link.addEventListener("click", () => {
+    if (activeLink != i) {
+      links[activeLink].classList.remove("active");
+      link.classList.add("active");
+      sections[activeLink].classList.remove("active");
+
+      setTimeout(() => {
+        activeLink = i;
+        sections[i].classList.add("active");
+      }, 0);
+    }
+  });
+});
+
+form.addEventListener("submit", (e) => {
+  form.reset();
+});
